2f9849e30da1101884cfe53110bf1e00989bccfe
Md Shoharab Pk <Md Shoharab Pk>
2021-02-02 08:48:52 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,23 @@
+
+//falsy
+// undefind
+//null
+//NaN 
+//0
+//''
+//false
+
+//truthy
+// ' ', '0', [], {}
+// true
+
+
+//var name = "shoharab";
+let name = [];
+console.log(name);
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
