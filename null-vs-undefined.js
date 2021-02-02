56f3158ab0cb9c7155db913a79a9d7d4d581a821
Md Shoharab Pk <Md Shoharab Pk>
2021-02-02 09:06:23 +0600

    null vs undefined clear for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+// let pakhi;
+// console.log(pakhi);  // undefined
+
+// function add(num1, num2) {
+//     console.log(num1+num2);
+// }
+// const result = add(15, 74);
+// console.log("result : ", result);
+
+// function add(num1, num2) {
+//     console.log(num1, num2);
+// }
+// const result = add(15);
+// console.log("result : ", result);
+
+const premik = {name: "janina", phone: 458265};
+console.log(premik.gf);
